23ae67b5f017a1082356ca5ffccec30ccbf9ebd9
MrFibunacci <mrfibuancci@gmail.com>
2025-02-16 02:15:04 +0100

    add alu and jump opcodes

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 u_int8_t memory[MEMORY_MAX];
 u_int8_t ram[MEMORY_MAX];
 
-enum Registers
+enum Reg
 {
 	A, 		// Akkumulator
 	D,		// Datenregister
@@ -19,10 +19,10 @@ enum Registers
 
 enum Flags
 {
-	B = 1 << 0,
-	C = 1 << 1,
-	E = 1 << 2,
-	Z = 1 << 3
+	B = 1 << 3,	// borrow (overflow on subtraction)
+	C = 1 << 2,	// carry (overflow on addition)
+	G = 1 << 1,	// greater (on cmp)
+	E = 1 << 0  // equal (on cmp)
 };
 
 enum Opcodes
@@ -67,7 +67,7 @@ enum ALU_Opcodes
 };
 
 // the registers actually only hold a nibble but c++ can't represent that.
-u_int8_t registers[Registers::COUNT];
+u_int8_t registers[Reg::COUNT];
 
 void read_image_file(FILE* file)
 {
@@ -106,14 +106,14 @@ u_int8_t ram_read(u_int8_t address)
 // return both Pointer registers as one 8-Bit value
 u_int8_t pointerHL()
 {
-	return (u_int8_t) (registers[Registers::PH] << 4) + registers[Registers::PL];
+	return (u_int8_t) (registers[Reg::PH] << 4) + registers[Reg::PL];
 }
 
 // save 8-Bit val to both Pointer registers
 void pointerHL_store(u_int8_t val)
 {
-	registers[Registers::PH] = val >> 4;
-	registers[Registers::PL] = val & 0x0f;
+	registers[Reg::PH] = val >> 4;
+	registers[Reg::PL] = val & 0x0f;
 }
 
 void autoIncDecPointer(u_int8_t E)
@@ -138,6 +138,27 @@ void printRAM()
 		if(i%16 == 15) {printf("\n");}
 	}
 	printf("\n\n");
+}
+
+void setFlag(Flags flag, bool val = 1) 
+{
+	if(val) {
+		registers[Reg::FLG] = registers[Reg::FLG] | flag;
+	} else {
+		registers[Reg::FLG] = registers[Reg::FLG] & ~(flag);
+	}
+}
+
+bool getFlag(Flags flag)
+{
+	u_int8_t flagPos;
+
+	if (flag == Flags::B) {flagPos = 3;}
+	else if (flag == Flags::C) {flagPos = 2;}
+	else if (flag == Flags::G) {flagPos = 1;}
+	else if (flag == Flags::E) {flagPos = 0;}
+
+	return (registers[Reg::FLG] >> flagPos) & 0x1;
 }
 
 int main(int argc, const char* argv[])
@@ -165,24 +186,26 @@ int main(int argc, const char* argv[])
 
 	bool isRunning = true;
 
-	registers[Registers::PC] = 0;
+	registers[Reg::PC] = 0;
 	
 	while (isRunning)
 	{
-		u_int8_t instruction = memory_read(registers[Registers::PC]++);
+		u_int8_t instruction = memory_read(registers[Reg::PC]++);
 		u_int8_t opcode = instruction >> 4;
 		u_int8_t E1 = (instruction & 0x0c) >> 2;
 		u_int8_t E2 = instruction & 0x03;
 
 		system("clear");
 		printRAM();
-		printf("Reg A: %01X\n", registers[Registers::A]);
-		printf("Reg d: %01X\n", registers[Registers::D]);
+		printf("Reg A: %01X\n", registers[Reg::A]);
+		printf("Reg d: %01X\n", registers[Reg::D]);
 		printf("Reg PH/L: %02X", pointerHL());
 		printf(" - Adress value: %01X\n", ram_read(pointerHL()));
-		printf("Reg SP: %01X\n", registers[Registers::SP]);
-		printf("Reg PC: %01X\n", registers[Registers::PC]);
-		printf("Reg FLG: %01X\n", registers[Registers::FLG]);
+		printf("Reg SP: %01X\n", registers[Reg::SP]);
+		printf("Reg PC: %01X\n", registers[Reg::PC]);
+		printf("Reg FLG: %01X\n", registers[Reg::FLG]);
+		printf("B C G E\n");
+		printf("%i %i %i %i", getFlag(Flags::B), getFlag(Flags::C), getFlag(Flags::G), getFlag(Flags::E));
 		printf("\nInst: %02X\n", instruction);
 		printf("E1: %i ", E1);
 		printf("E2: %i\n", E2);
@@ -194,7 +217,7 @@ int main(int argc, const char* argv[])
 			break;
 
 		case Opcodes::INA:
-			registers[Registers::A] = instruction & 0x0F;
+			registers[Reg::A] = instruction & 0x0F;
 			break;
 
 		case Opcodes::MOV:
@@ -210,7 +233,79 @@ int main(int argc, const char* argv[])
 			registers[E1] = ram_read(pointerHL());
 			autoIncDecPointer(E2);
 			break;
-		
+
+		case Opcodes::ALU:
+			switch ((E1 << 2)+E2)
+			{
+			case ALU_Opcodes::ADD:
+				registers[Reg::A] = registers[Reg::A] + registers[Reg::D];
+				if (registers[Reg::A] >> 4 == true)  { setFlag(Flags::C); }
+				registers[Reg::A] = registers[Reg::A] & 0x0f;
+				break;
+			case ALU_Opcodes::SUB:
+				registers[Reg::A] = registers[Reg::A] - registers[Reg::D];
+				if (registers[Reg::A] >> 4 & 0x1 == true)  { setFlag(Flags::B); }
+				registers[Reg::A] = registers[Reg::A] & 0x0f;
+				break;
+			case ALU_Opcodes::INC:
+				registers[Reg::A] = registers[Reg::A] + 1;
+				if (registers[Reg::A] >> 4 == true)  { setFlag(Flags::C); }
+				registers[Reg::A] = registers[Reg::A] & 0x0f;
+				break;
+			case ALU_Opcodes::DEC:
+				registers[Reg::A] = registers[Reg::A] - 1;
+				if (registers[Reg::A] >> 4 & 0x1 == true)  { setFlag(Flags::B); }
+				registers[Reg::A] = registers[Reg::A] & 0x0f;
+				break;
+			case ALU_Opcodes::CMP:
+				if (registers[Reg::A] > registers[Reg::D]) { setFlag(Flags::G); }
+				if (registers[Reg::A] == registers[Reg::D]) { setFlag(Flags::E); }
+				break;
+			case ALU_Opcodes::NOT:
+				registers[Reg::A] = ~registers[Reg::A];
+				break;
+			case ALU_Opcodes::AND:
+				registers[Reg::A] = registers[Reg::A] & registers[Reg::D];
+				break;
+			case ALU_Opcodes::NAND:
+				registers[Reg::A] = ~(registers[Reg::A] & registers[Reg::D]);
+				break;
+			case ALU_Opcodes::OR:
+				registers[Reg::A] = registers[Reg::A] | registers[Reg::D];
+				break;
+			case ALU_Opcodes::NOR:
+				registers[Reg::A] = ~(registers[Reg::A] | registers[Reg::D]);
+				break;
+			case ALU_Opcodes::XOR:
+				registers[Reg::A] = registers[Reg::A] ^ registers[Reg::D];
+				break;
+			case ALU_Opcodes::XNOR:
+				registers[Reg::A] = ~(registers[Reg::A] ^ registers[Reg::D]);
+				break;
+			case ALU_Opcodes::SHL:
+				registers[Reg::A] = registers[Reg::A] << registers[Reg::D];
+				break;
+			case ALU_Opcodes::SHR:
+				registers[Reg::A] = registers[Reg::A] >> registers[Reg::D];
+				break;
+			case ALU_Opcodes::ROL:
+				registers[Reg::A] = (registers[Reg::A] << registers[Reg::D])|(registers[Reg::A] >> (4 - registers[Reg::D]));
+				break;
+			case ALU_Opcodes::ROR:
+				registers[Reg::A] = (registers[Reg::A] >> registers[Reg::D])|(registers[Reg::A] << (4 - registers[Reg::D]));
+				break;
+			default:
+				break;
+			}
+			break;
+
+		case Opcodes::JIT:
+			if (getFlag((Flags)((E1 << 2)+E2))) { registers[Reg::PC] = pointerHL(); }
+			break;
+			// TODO: should flag be reset after jump occours?
+		case Opcodes::JIF:
+			if (!getFlag((Flags)((E1 << 2)+E2))) { registers[Reg::PC] = pointerHL(); }
+			break;
 		case Opcodes::HLT:
 			isRunning = false;
 			break;
@@ -219,7 +314,7 @@ int main(int argc, const char* argv[])
 			break;
 		}
 
-		if (registers[Registers::PC] > 255)
+		if (registers[Reg::PC] > 255)
 		{
 			isRunning = false;
 		}
